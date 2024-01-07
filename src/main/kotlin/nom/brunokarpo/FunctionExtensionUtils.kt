cdc461327f2cb68d7365c56a0b3d30fcfcbdaa3a
Bruno Nogueira <ti.brunonogueira@gmail.com>
2024-01-07 17:06:06 -0300

    Removed unused class

--- src/main/kotlin/nom/brunokarpo/FunctionExtensionUtils.kt
+++
@@ -1,8 +0,0 @@
-package nom.brunokarpo
-
-import java.math.BigDecimal
-import java.math.RoundingMode
-
-fun BigDecimal.toDefaultScale() : BigDecimal {
-    return this.setScale(2, RoundingMode.DOWN)
-}
